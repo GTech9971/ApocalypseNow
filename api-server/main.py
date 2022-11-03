393971accea51df43a501e65b0975352f52382e5
GTech9971 <98648140+GTech9971@users.noreply.github.com>
2022-11-04 02:11:18 +0900

    api-serverようにyoloロジックを書き換え

--- api-server/main.py
+++ api-server/main.py
@@ -2,11 +2,13 @@ from pathlib import Path
 
 from fastapi import FastAPI, UploadFile, File
 
-import detect
+import detect_targetsite
 
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # api-server root directory
+
+UPLOAD_TARGETSITE_PATH = Path(ROOT, "upload", "targetsite") # アップロードされた的本体の画像データ
 
 TARGET_SITE_WEIGHT_PATH = Path(ROOT, "weights", "targetsite.pt")  # 的本体の学習結果データ
 SITE_WEIGHT_PATH = Path(ROOT, "weights", "site.pt")  # 的の学習結果データ
@@ -28,9 +30,9 @@ def read_item(item_id: int, q: str = None):
 
 @app.post("/upload_target")
 def upload_target(file: UploadFile = File(...)):
-    try:
+    try:        
         contents = file.file.read()
-        save_path = Path(ROOT, "upload", "targetsite", file.filename)
+        save_path = Path(UPLOAD_TARGETSITE_PATH, file.filename)
         with open(save_path, "wb") as f:
             f.write(contents)
     except Exception as e:
@@ -39,10 +41,10 @@ def upload_target(file: UploadFile = File(...)):
         file.file.close()
 
     # yoloで座標取得
-    detect.run(weights=TARGET_SITE_WEIGHT_PATH,
-               source=save_path, save_txt=True)
+    detect_targetsite.run(weights=TARGET_SITE_WEIGHT_PATH,
+               source=save_path, save_txt=True, exist_ok=True)
     # 出力結果を読み取る
-    label_path = Path(YOLO_APP_ROOT, "runs/detect/exp/label/",
+    label_path = Path(YOLO_APP_ROOT, "runs/detect/exp/labels/",
                       file.filename.replace(".png", ".txt"))
     result = load_label(label_path)
 
@@ -54,10 +56,22 @@ def upload_target(file: UploadFile = File(...)):
 '''
 
 
-def load_label(path: str):
+def load_label(label_path: str):
     try:
-        with open(path, "r") as f:
-            line = f.readline()
-            return line
+        with open(label_path, "r") as f:         
+            result = f.readlines()
+            if(len(result) == 0):
+                return []
+
+            yolo_data = result[0].split(" ")
+            label = int(yolo_data[0])
+
+            x = float(yolo_data[1])
+            y = float(yolo_data[2])
+            w = float(yolo_data[3])
+            h = float(yolo_data[4])
+            
+            return [label, x, y, w, h]
+
     except Exception as e:
         return e
