f410b76af08e06ae08641d95d6e55ab8807e72e6
wsl-dev <georgetech1984@gmail.com>
2022-11-12 21:20:57 +0900

    target_sitesテーブルの設計を修正

--- api-server/initdb/create_tbl.sql
+++ api-server/initdb/create_tbl.sql
@@ -1,9 +1,15 @@
 /** 的の情報 */
 DROP TABLE IF EXISTS target_sites;
 CREATE TABLE target_sites(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     img_path VARCHAR(256),
-    created_at DATETIME    
+    hit_img_path VARCHAR(256),
+    created_at DATETIME,
+    updated_at DATETIME,
+    trim_x INT,
+    trim_y INT,
+    trim_w INT,
+    trim_h INT
 );
 
 /** 的のヒットポイント */
@@ -14,6 +20,7 @@ CREATE TABLE target_site_hit_points(
     y INT NOT NULL,
     hit_point INT NOT NULL,
     created_at DATETIME,
-    FOREIGN KEY (target_site_id) REFERENCES target_sites(id) 
+    FOREIGN KEY (target_site_id) REFERENCES target_sites(id)         
         ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
