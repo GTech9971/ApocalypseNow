551191f1868052e7d0eb734f1f8dcc6840a2ef7c
wsl-dev <georgetech1984@gmail.com>
2022-11-16 03:08:40 +0900

    キャッシュ回避できなかったので戻した

--- api-server/build-image.sh
+++ api-server/build-image.sh
@@ -1,3 +1,1 @@
-#docker build -t george9971/ap2n:latest . --no-cache
-# git cloneキャッシュ対策でdummyファイルを作成する
-touch dummyfile && docker build -t george9971/ap2n:latest .
+docker build -t george9971/ap2n:latest . --no-cache
